9512965d716c52b34e5289f983c8954cfa190522
HaydenZain <hzd_btest@163.com>
2017-11-07 11:35:24 +0800

    update jvm_gc_info

--- db.py
+++ db.py
@@ -34,8 +34,13 @@ class DB():
         disk_sql = "INSERT INTO disk(host_name, ip, create_time, Device, rrqm_s, wrqm_s, r_s, w_s, rkB_s, wkB_s, avgrq_sz, avgqu_sz, await, svctm, util) " \
                    "VALUES (%(host_name)s, %(ip)s, %(create_time)s, %(Device)s, %(rrqm_s)s, %(wrqm_s)s, %(r_s)s, %(w_s)s, %(rkB_s)s, %(wkB_s)s, %(avgrq_sz)s, %(avgqu_sz)s, %(await)s, %(svctm)s, %(util)s)"
 
+        net_sql = "INSERT INTO net(host_name, ip, create_time, adapter_name, re_bytes, re_packets, re_errors, re_drops, tr_bytes, tr_packets, tr_errors, tr_drops) " \
+                  "VALUES (%(host_name)s, %(ip)s, %(create_time)s, %(adapter_name)s, %(re_bytes)s, %(re_packets)s, %(re_errors)s, %(re_drops)s, %(tr_bytes)s, %(tr_packets)s, %(tr_errors)s, %(tr_drops)s)"
 
-        all_sql = [cpu_sql,mem_sql,disk_sql]
+        jvm_gc_sql = "INSERT INTO jvm_gc (host_name, ip, create_time, PID, name_path, S0C, S1C, S0U, S1U, EC, EU, OC, OU, PC, PU, YGC, YGCT, FGC, FGCT, GCT) " \
+                     "VALUES (%(host_name)s, %(ip)s, %(create_time)s, %(PID)s, %(name_path)s, %(S0C)s, %(S1C)s, %(S0U)s, %(S1U)s, %(EC)s, %(EU)s, %(OC)s, %(OU)s, %(PC)s, %(PU)s, %(YGC)s, %(YGCT)s, %(FGC)s, %(FGCT)s, %(GCT)s)"
+
+        all_sql = [cpu_sql,mem_sql,disk_sql,net_sql,jvm_gc_sql]
         try:
             for sql in all_sql:
                 if "cpu" in sql:
@@ -47,6 +52,18 @@ class DB():
                     if disk_io_datas:
                         for dev in disk_io_datas.keys():
                             cursor.execute(sql,disk_io_datas[dev])
+                if "net" in sql:
+                    net_io_datas = self.info.get_net()
+                    #print net_io_datas
+
+                    if net_io_datas:
+                        for dev in net_io_datas.keys():
+                            cursor.execute(sql,net_io_datas[dev])
+                if "jvm_gc" in sql:
+                    jvm_gc_datas = self.info.get_jvm_gc()
+                    if jvm_gc_datas:
+                        for pid in jvm_gc_datas.keys():
+                            cursor.execute(sql,jvm_gc_datas[pid])
             self.conn.commit()
         except mysql.connector.Error as e:
             print "commit fails!{}".format(e)
