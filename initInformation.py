7ef5f43b0d3bacd5331ee9788840d7f8a400c605
HaydenZain <hzd_btest@163.com>
2018-01-03 13:06:01 +0800

    增加java服务判断

--- initInformation.py
+++ initInformation.py
@@ -6,6 +6,13 @@ import os
 def check_services():
     services_status = {} #1:启动  0:关闭
     isredis = os.popen("ps -ef|grep redis | grep -v grep").readlines()
-    if isredis:services_status['redis'] = 1
-    else:services_status['redis'] = 0
+    isjava = os.popen("ps -ef|grep java | grep -v grep").readlines()
+    if isjava:
+        services_status['java'] = 1
+    else:
+        services_status['java'] = 0
+    if isredis:
+        services_status['redis'] = 1
+    else:
+        services_status['redis'] = 0
     return services_status
